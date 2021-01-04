82c37b1e1e6b919e355caf06c6b0e58dea266404
Jordan Oneal <jloburnley02@aol.com>
2021-01-04 12:01:04 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        I made a Change!!!
         <a
           className="App-link"
           href="https://reactjs.org"
